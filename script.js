b72fddf27bede874255908a2d2af5590a6e0d307
danfireman <dyth68@yahoo.com>
2020-07-19 11:26:21 +0100

    Now actually works with cards in hand

--- script.js
+++ script.js
@@ -6,15 +6,15 @@
 // @grant    none
 // ==/UserScript==
 
-if (document.URL.includes("raceforthegalaxy")) {
+if (document.URL.includes("raceforthegalaxy") || document.URL.includes("replay")) {
   var htmlSource = ""
-  console.log("cxh script loaded!");
+  console.log("RftG script loaded!");
   function myOnloadFunc() {
       var xmlHttp = new XMLHttpRequest();
       xmlHttp.onreadystatechange = function() { 
           if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             //html = $.parseHTML( xmlHttp.responseText );
-            console.log("dundundun");
+            console.log("Grabbed racepics page");
             htmlSource = xmlHttp.responseText;
           }
       }
@@ -26,18 +26,21 @@ if (document.URL.includes("raceforthegalaxy")) {
 
   function improveImages() {
     if (htmlSource == "") {
-      console.log("sad");
+      console.log("awaiting racepics html load");
     }
     else {
       try {
-        for (const card of document.querySelectorAll(".card")) {
+        for (const card of document.querySelectorAll(".card,.stockitem")) {
           if (!card.getAttribute("style").includes("racepics"))  {
-            cardname=card.querySelectorAll(".cardname")[0].textContent;
-            console.log(cardname);
+            var cardname=card.querySelectorAll(".cardname")[0].textContent;
             if (htmlSource.includes(cardname)) {
-              picloc = htmlSource.split(cardname)[1].split("alt")[0].split("src='")[1].split("'")[0];
-              console.log(picloc);
-              card.setAttribute("style", "background-image:url(http://racepics.weihwa.com/" + picloc + "); background-size: 126px 174px; background-position: -7px -7px;");
+              var picloc = htmlSource.split(cardname)[1].split("alt")[0].split("src='")[1].split("'")[0];
+              if (card.getAttribute("class").includes("stockitem")) {
+                card.setAttribute("style", card.getAttribute("style") + "background-image:url(http://racepics.weihwa.com/" + picloc + "); background-size: 126px 174px; background-position: -7px -7px;");
+              }
+              else {
+              	card.setAttribute("style", "background-image:url(http://racepics.weihwa.com/" + picloc + "); background-size: 126px 174px; background-position: -7px -7px;");
+              }
               console.log("card pic for \"" + cardname + "\" set " + picloc);
             }
           }
@@ -52,5 +55,5 @@ if (document.URL.includes("raceforthegalaxy")) {
 
   setInterval(improveImages, 4000);
 
-  console.log("cxh script initialized!");
+  console.log("RftG script initialized!");
 }
